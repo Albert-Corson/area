a2707f2054044fd835c3fd33848e62628666235d
adrienlucbert <adrien.lucbert@epitech.eu>
2020-11-29 23:49:14 +0100

    add(client): widget page

---
+++ client/pages/services/_id/widgets/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page widget">
+    {{ id }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'Widget',
+  components: {
+  },
+  validate({ params }) {
+    const id = parseInt(params.id)
+    return !isNaN(id)
+  }
+})
+export default class WidgetPage extends Vue {
+  public id?: number
+
+  async beforeCreate() {
+    this.id = parseInt(this.$route.params.id)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
