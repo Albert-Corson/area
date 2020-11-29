e41c2aebd72351b597e007435dfdc743291e6c05
math.vgr <mathias.vigier@epitech.eu>
2020-11-29 14:41:43 +0100

    [ADD] navbar, footer, header, body element

---
+++ client/components/icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="icon">
+    <img :src="src" alt="icon button" :width="width" :height="height"/>
+  </div>
+</template>
+
+
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component({
+  name: 'Icon',
+  components: {
+  }
+})
+export default class Icon  extends Vue {
+  // data
+    @Prop({required: true}) readonly src: string
+    @Prop({required: true}) readonly width: number
+    @Prop({required: true}) readonly height: number
+}
+</script>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,42 +1,75 @@
 <template>
   <div class="default">
-    <router-link to="/login">To Login</router-link>
-    <nuxt/>
+    <dashboard-nav-bar />
+    <dashboard-header />
+    <!-- <router-link to="/login">To Login</router-link> -->
+    <nuxt />
+    <dashboard-footer />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import DashboardNavBar from "../components/navbar/navbar.vue"
+import DashboardFooter from "../components/footer/footer.vue"
+import DashboardHeader from "../components/header/header.vue"
 
 @Component({
   name: 'DefaultLayout',
   components: {
+    DashboardNavBar,
+    DashboardFooter,
+    DashboardHeader
   }
 })
 export default class DefaultLayout extends Vue {
   // data
-  items = [
-    {
-      icon: 'mdi-apps',
-      title: 'Welcome'
-    }
-  ] 
-  inputUser: string = ""
 
-  @Prop(String) readonly title: string
-
-  public get sortedItems() {
-    return this.items
-  }
-
-  toto(value) {
-
-  }
 }
 </script>
 
-<style scoped>
+<style>
 :root {
-
+  --main-bg-color: #1B2224;
+  --secondary-bg-color: #222B2E;
+  --focus-color: #2eb3982a;
+  --active-color: #E4E4E4;
+  --inactive-color: #474747;
+  --input-radius: 5px;
 }
+
+/* todo create image that follow cursor */
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  color: var(--active-color);
+}
+
+.clickable {
+  cursor: pointer;
+}
+
+</style>
+
+<style scoped>
+
+.default {
+  background-color: var(--main-bg-color);
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-columns: 0.75fr 5fr;
+  grid-template-rows: 0.75fr 10fr 0.75fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    "header header"
+    "nav-bar main"
+    "footer footer";
+}
+
 </style>
