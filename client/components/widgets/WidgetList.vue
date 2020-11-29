1aaccf3270f13a55a8ce6d9aa5e1f1b4c1b3afbe
adrienlucbert <adrien.lucbert@epitech.eu>
2020-11-29 23:25:25 +0100

    add(client): widgets list

---
+++ client/components/widgets/WidgetList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="widget-list">
+    <mosaic-list>
+      <widget-list-item v-for="widget in widgets" :key="widget.id"
+               :id="widget.id"
+               :name="widget.name"
+               :service="widget.service"
+               />
+    </mosaic-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import MosaicList from '~/components/MosaicList.vue'
+import WidgetListItem from '~/components/widgets/WidgetListItem.vue'
+import WidgetModel from '~/api/models/WidgetModel'
+
+@Component({
+  name: 'WidgetList',
+  components: {
+    MosaicList,
+    WidgetListItem
+  }
+})
+export default class WidgetList extends Vue {
+  @Prop({ required: true }) readonly widgets!: Array<WidgetModel>
+}
+
+</script>
