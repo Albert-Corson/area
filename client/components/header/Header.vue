d82160ffc5006e460634c532542901ae86b6f423
math.vgr <mathias.vigier@epitech.eu>
2020-11-30 01:48:40 +0100

    [ADD] Header with header glitter that is basicly breadCrumbs that does nothings;[MOD] path of svg add /

--- client/components/header/Header.vue
+++ client/components/header/Header.vue
@@ -1,11 +1,50 @@
 <template>
-    <div class="header"/>
+    <div class="header">
+      <header-button/>
+      <header-glitter/>
+    </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import HeaderButton from "~/components/header/HeaderButton.vue"
+import HeaderGlitter from "~/components/header/HeaderGlitter.vue"
+
+@Component({
+  name: 'Header',
+  components: {
+    HeaderButton,
+    HeaderGlitter,
+  }
+})
+export default class Header extends Vue {
+ 
+}
+</script>
 
 <style scoped>
 
-.header {
+.content
+{
+  grid-area: content;
+  border: solid 1px black
+}
+
+.header
+{
   grid-area: header;
+}
+
+.header {
+  background-color: var(--main-bg-color);
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 0.75fr 5fr;
+  grid-template-rows: 1fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    "header-button header-glitter";
 }
 
 </style>
