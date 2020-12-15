ca2d6b84c2f8ef3cac1b93de76c6bbed2eb158ac
adrienlucbert <adrien.lucbert@epitech.eu>
2020-12-15 12:27:01 +0100

    feat: widget 12: random dad jokes

--- client/components/widgets-applets/WidgetApplet12.vue
+++ client/components/widgets-applets/WidgetApplet12.vue
@@ -1,19 +1,71 @@
 <template>
-  <div class="widget-applet-12">
-    <h1>Widget Applet 12</h1>
-  </div>
+  <v-app class="widget-applet-12">
+    <h1>{{ widget.name }}</h1>
+    <div v-if="item" class="joke">
+      {{ item.content }}
+    </div>
+    <v-btn
+      elevation="2"
+      @click="reload"
+      >
+      Another one
+    </v-btn>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import WidgetModel from '~/api/models/WidgetModel'
+import { WidgetStore } from '~/store'
+
+interface JokeModel {
+  content: string
+}
 
 @Component({
   name: 'WidgetApplet12',
-  components: {
-  }
 })
 export default class WidgetApplet12 extends Vue {
+  // data
+  public item: JokeModel | null = null
+
+  // props
+  @Prop({ required: true }) readonly widget!: WidgetModel
+
+  // methods
+  public async reload() {
+    const res = await WidgetStore.fetchWidgetData({
+      widgetId: this.widget.id
+    })
+    console.log(res)
+    if (!res.item) return
+    this.item = res.item
+  }
+
+  // lifecycle
+  beforeMount() {
+    this.reload()
+  }
 }
 
 </script>
 
+<style lang="scss" scoped>
+h1 {
+  margin-bottom: 1em;
+}
+
+.mosaic-list {
+  margin-top: 1em;
+}
+
+.preview-image {
+  width: 150px;
+  height: 150px;
+  background-size: cover;
+}
+
+.joke {
+  margin-bottom: 2em;
+}
+</style>
