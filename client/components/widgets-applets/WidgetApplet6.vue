095728a2ad339179a564ea0f3f4d4e545a1a2d6c
adrienlucbert <adrien.lucbert@epitech.eu>
2020-12-15 15:16:12 +0100

    wip: widget 6 boilerplate

--- client/components/widgets-applets/WidgetApplet6.vue
+++ client/components/widgets-applets/WidgetApplet6.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="widget-applet-6">
-    <h1>Widget Applet 6</h1>
-  </div>
+  <v-app class="widget-applet-6">
+    <h1>{{ widget.name }}</h1>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import WidgetModel from '~/api/models/WidgetModel'
+import { WidgetStore } from '~/store'
 
 @Component({
   name: 'WidgetApplet6',
@@ -13,7 +15,34 @@ import { Component, Vue } from 'nuxt-property-decorator'
   }
 })
 export default class WidgetApplet6 extends Vue {
+  // data
+
+  // props
+  @Prop({ required: true }) readonly widget!: WidgetModel
+
+  // methods
+  public async reload() {
+    const res = await WidgetStore.fetchWidgetData({
+      widgetId: this.widget.id
+    })
+    console.log(res)
+    // update data
+  }
+
+  // lifecycle
+  beforeMount() {
+    this.reload()
+  }
 }
 
 </script>
 
+<style lang="scss" scoped>
+h1 {
+  margin-bottom: 1em;
+}
+
+.mosaic-list {
+  margin-top: 1em;
+}
+</style>
