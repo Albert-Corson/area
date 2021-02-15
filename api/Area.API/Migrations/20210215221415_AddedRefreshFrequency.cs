01f7e7f9482cfc41602451c2b4c40b776e39874c
Albert Corson <corsonalbert@gmail.com>
2021-02-15 23:37:12 +0100

    feat(api): added a recommended refresh frequency value to widgets

---
+++ api/Area.API/Migrations/20210215221415_AddedRefreshFrequency.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Area.API.Migrations
+{
+    public partial class AddedRefreshFrequency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Frequency",
+                table: "Widgets",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Frequency",
+                table: "Widgets");
+        }
+    }
+}
