06a072872b0f28fd7c8f449b11d7a7db7c91dfad
Louis Viot <louis.viot@epitech.eu>
2021-03-03 10:37:03 +0100

    mod: better fatal error proof

--- mobile/src/Components/DraggableContainer.tsx
+++ mobile/src/Components/DraggableContainer.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useRef, useState, useEffect} from 'react'
+import React, {useContext, useRef} from 'react'
 import {
   Dimensions, StyleSheet, View, TouchableOpacity,
 } from 'react-native'
