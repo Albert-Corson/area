e8c337b8f741d109a14a0cc5a259ee687f455634
adrienlucbert <adrien.lucbert@epitech.eu>
2021-03-17 01:59:47 +0100

    feat: better top navigation bar

--- desktop/src/components/NavMenu.vue
+++ desktop/src/components/NavMenu.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="nav-menu">
-    <div class="links">
-      <router-link to="/">
-        Dashboard
-      </router-link>
-      <router-link to="/auth/signout">
+    <div class="nav-left">
+      <h2 class="nav-button">AREA</h2>
+    </div>
+    <div class="nav-center"></div>
+    <div class="nav-right">
+      <div class="nav-button">
+        {{ username }}
+      </div>
+      <router-link to="/auth/signout" class="nav-button">
         Sign out
       </router-link>
     </div>
@@ -14,6 +18,11 @@
 <script>
 export default {
   name: "nav-menu",
+  created() {
+    if (this.$store.state.User.username === null) {
+      this.$store.dispatch("User/fetchInfo")
+    }
+  },
   computed: {
     username() {
       return this.$store.state.User.username?.slice(
@@ -29,6 +38,7 @@ export default {
 @import "@/styles/vars";
 
 .nav-menu {
+  overflow: hidden;
   z-index: 99;
   line-height: 4em;
   width: 100%;
@@ -40,17 +50,30 @@ export default {
   box-shadow: $upShadow;
   color: #fdfdfd;
   font-size: 1.2em;
+  display: flex;
 
-  .profile {
-    height: 4rem;
-    padding: 1rem;
-    background: #282828;
+  .nav-center {
+    flex: 1;
   }
 
-  .links > * {
-    color: inherit;
-    text-decoration: none;
-    padding: 1em;
+  .nav-button {
+    cursor: pointer;
+    display: inline-block;
+    height: 4em;
+    margin: 0;
+    padding: 0 2rem;
+
+    box-shadow: inset 0 0 0 0 $accentColor;
+    transition: box-shadow 0.2s ease-out;
+    &:hover {
+      box-shadow: inset 0 -8px 0 0 $accentColor;
+    }
+
+    &,
+    * {
+      color: inherit;
+      text-decoration: none;
+    }
   }
 }
 </style>
